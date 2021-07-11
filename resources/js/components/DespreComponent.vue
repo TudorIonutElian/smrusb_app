7a1371ecde0d6aa64f20ca6553358d99fc8b14db
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2021-07-11 17:08:06 +0300

    Actualizare pagina despre

--- resources/js/components/DespreComponent.vue
+++ resources/js/components/DespreComponent.vue
@@ -28,8 +28,201 @@
                                 Sistemul informatic este cuprins din codul sursă, baza de date precum și resursele web alocate acestuia, respectiv imagini și alte fișiere. Este dezvoltat pe arhitectura orientată obiect, iar fiecare tabel existent în baza de date destinată va avea o clasă PHP alocată, sub denumirea de Model (de exemplu tabelul angajați are modelul definit în clasa Angajat.php).
                                 Orice clasă de tip model conține conform documentației oficiale Laravel, denumirea tabelului, precum și proprietăți ale acesteia sau metode, pentru relaționarea cu alte clase, respectiv tabele ale bazei de date. Metodele create în interiorul claselor de tip model, ce fac legătura dintre tabel și obiect, permit relaționarea ușoara de tip inner join, fără a scrie interogări foarte lungi și greu de optimizat, permițând returnarea datelor din legătura ca proprietăți ale obiectului (exemplu Angajat->institutie_curenta).php
                             </div>
-                            <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cumque fuga, ipsam odio soluta temporibus? Animi assumenda consectetur cum ea excepturi fuga, fugiat neque velit vero! Alias aliquam aperiam beatae cum dignissimos dolore error harum, illo illum ipsa iure laudantium libero, maxime nobis nulla perferendis, quos rerum sapiente velit veniam!</div>
-                            <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">...</div>
+                            <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <div>1. Administrator</div>
+                                <div>2. Specialist Resurse Umane</div>
+                                <div>3. Angajat</div>
+                            </div>
+                            <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Angajati
+                                                </h5>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    <span>Adaugare angajati</span>
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    <span>Numire Angajati</span>
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Mutare angajati
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Incetare contract angajati
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Evaluare
+                                                </h5>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare Evaluare angajati
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Adaugare Evaluare angajati
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Aprobare Evaluare angajati
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare contestatii angajati
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare & Adaugare recompense angajati
+                                                </div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare & Adaugare sanctiuni angajati
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Stat de Organizare
+                                                </h5>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare Stat de Organizare</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Suplimentare Stat de Organizare</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Radiere pozitii</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare posturi vacante</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Adaugare cuprins</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Salarii
+                                                </h5>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Generare Salarii</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare salarii</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Situatii Statistice
+                                                </h5>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie incetari contracte</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie mutari de personal</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie Salarii Lunare</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie Salarii Trimestriale</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie Salarii Semestriale</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie Salarii Anuale</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie CASS Achitat</div>
+                                                <div>
+                                                    <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Situatie CASS Neachitat</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Pontaj
+                                                </h5>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Aprobare pontaj</div>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Vizualizare pontaj</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Documente
+                                                </h5>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Adeverinta angajat</div>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Adeverinta asigurat</div>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Adeverinta salarii</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="card mt-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title bg-primary text-white text-center p-2">
+                                                    Modul Panou Control
+                                                </h5>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Schimbare parola</div>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Solicitare acces</div>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Aprobare cont angajat</div>
+                                                <div>
+                                                     <img class="m-1" src="/images/verificat.png" alt="">
+                                                    Aprobare pontaj</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">...</div>
                         </div>
                     </div>
